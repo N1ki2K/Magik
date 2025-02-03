43f31bc3e8f7fe25ef26ed4be647325b6cfa12a5
N1ki2K <pan07nik@gmail.com>
2025-02-03 19:13:09 +0200

    test

---
+++ src/main/java/net/example/magik/item/ModItems.java
@@ -0,0 +1,11 @@
+package net.example.magik.item;
+
+import net.example.magik.Magik;
+import net.minecraft.references.Items;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Magik.MOD_ID);
+}
